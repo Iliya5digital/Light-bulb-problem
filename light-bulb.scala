6fe4ddf26a71b9e00fce3aa541a2010f9383e15f
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-11-15 18:53:17 +0300

    Create light-bulb.scala

---
+++ light-bulb.scala
@@ -0,0 +1,17 @@
+val totalExpenses = 61
+val room1Lamps = 10
+val room1Days = 21
+val room1Hours = 12
+val room2Lamps = 20
+val room2Days = 30
+val room2Hours = 8
+
+val totalLamps = room1Lamps + room2Lamps
+val totalDays = room1Days + room2Days
+val totalHours = room1Hours + room2Hours
+
+val room1Expenses = (room1Lamps.toDouble / totalLamps) * (room1Days.toDouble / totalDays) * (room1Hours.toDouble / totalHours) * totalExpenses
+val room2Expenses = (room2Lamps.toDouble / totalLamps) * (room2Days.toDouble / totalDays) * (room2Hours.toDouble / totalHours) * totalExpenses
+
+println(s"Expenses for Room 1: ${room1Expenses} rubles")
+println(s"Expenses for Room 2: ${room2Expenses} rubles")
