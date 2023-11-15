f756cbcf771701df70d7527ef0dae1c931020544
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-11-15 18:16:36 +0300

    Create light-bulb.cs

---
+++ light-bulb.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double totalExpenses = 61.0;
+        
+        int lampsRoom1 = 10;
+        int daysRoom1 = 21;
+        int hoursRoom1 = 12;
+        
+        int lampsRoom2 = 20;
+        int daysRoom2 = 30;
+        int hoursRoom2 = 8;
+        
+        double totalLamps = lampsRoom1 + lampsRoom2;
+        double totalDays = daysRoom1 + daysRoom2;
+        double totalHours = hoursRoom1 + hoursRoom2;
+        
+        double expensesRoom1 = (lampsRoom1 / totalLamps) * (daysRoom1 / totalDays) * (hoursRoom1 / totalHours) * totalExpenses;
+        double expensesRoom2 = (lampsRoom2 / totalLamps) * (daysRoom2 / totalDays) * (hoursRoom2 / totalHours) * totalExpenses;
+        
+        Console.WriteLine($"Расходы на освещение первого помещения: {expensesRoom1} рублей");
+        Console.WriteLine($"Расходы на освещение второго помещения: {expensesRoom2} рублей");
+    }
+}
